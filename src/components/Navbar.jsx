bfc99d0648d70f6386001496c6668653239ba82f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-03 00:12:01 +0200

    working in navbarr components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const Navbar = () => {
+
+  const navLinks = [
+    {href: '#home', label: 'Home'},
+    {href: '#about', label: 'About'},
+  ]
   return (
     <nav className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm border-gray-100 text-white z-50 shadow-sm'>
       <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-8 md:h-20 lg:px-8 h-16'>
