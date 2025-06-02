4cec29bf4d4b714db1953b4bae957eb1e25e005f
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-02 21:47:09 +0200

    working in navbarr components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
       <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-8 md:h-20 lg:px-8 h-16'>
             {/* logo */}
             <div className='flex items-center gap-1 cursor-pointer'>
-              <div className='w-4 h-4 bg-blue-600'></div>
+              <div className='w-4 h-4 bg-blue-600 rounded-full opacity-75 hover:opacity-100 transition-opacity'></div>
               <div></div>
             </div>
             {/* desktop navitems */}
