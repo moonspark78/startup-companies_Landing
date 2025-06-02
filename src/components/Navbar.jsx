17bc9eee750922337a8f2a05305573ad9911dbc3
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-02 21:46:10 +0200

    working in navbarr

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,10 @@ const Navbar = () => {
     <nav className='fixed top-0 left-0 right-0 bg-white/90 backdrop-blur-sm border-gray-100 text-white z-50 shadow-sm'>
       <div className='w-full container mx-auto flex items-center justify-between px-4 sm:px-8 md:h-20 lg:px-8 h-16'>
             {/* logo */}
+            <div className='flex items-center gap-1 cursor-pointer'>
+              <div className='w-4 h-4 bg-blue-600'></div>
+              <div></div>
+            </div>
             {/* desktop navitems */}
             {/* get in touch btn */}
             {/* mobile menu */}
