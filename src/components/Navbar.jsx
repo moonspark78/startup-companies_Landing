c383687fb87a1278b1094ba810d3625ae482eff4
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-04 00:48:27 +0200

    working in Navbar Components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             {/* get in touch btn */}
             <button className='hidden md:block bg-blue-600 text-white px-6 py-2.5 rounded-lg hover:bg-blue-700 text-sm
             font-medium transition-all hover:shadow-lg hover:shadow-blue-100'>
-
+              <a href='#newsletter'>Get in Touch</a>
             </button>
             {/* mobile menu */}
       </div>
