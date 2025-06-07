bea16810870bb20e4b0dc48f58e6211eeb827371
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-07 11:32:43 +0200

    :art: working on  the Hero section  :art:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { FaStar } from "react-icons/fa";
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className='container mx-auto flex flex-col md:flex-row justify-between items-center pt-44 pb-6 px-4 sm:px-6 lg:px-8'>
+        Hero
+    </section>
   )
 }
 
