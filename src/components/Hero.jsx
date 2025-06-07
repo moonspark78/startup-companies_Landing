85a03f18af78f5b7e780dd500e9a0091ccaa5b76
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-08 01:50:10 +0200

    :art: working on  The Hero Section  :art:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
             {/* star badge */}
             <div className='flex items-center gap-2 bg-gray-50 w-fit px-4 py-2 rounded-full hover:bg-gray-100 transition-colors
             cursor-pointer group'>
-                <span className='text-blue-600 group-hover:scale-110 transition-transform'><FaStar /></span>
+                <span className='text-blue-600 group-hover:text-amber-300 group-hover:scale-110 transition-transform'><FaStar /></span>
                 <span className='text-sm font-medium'>Jump start your growth</span>
             </div>
         </div>
