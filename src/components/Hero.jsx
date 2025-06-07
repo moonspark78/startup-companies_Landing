e2cd0438342cd21c28d26e1e82fd1b8e2dde0d19
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-07 11:39:26 +0200

    :art: working on  The Hero Section  :art:

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,8 +5,17 @@ import { FaStar } from "react-icons/fa";
 const Hero = () => {
   return (
     <section className='container mx-auto flex flex-col md:flex-row justify-between items-center pt-44 pb-6 px-4 sm:px-6 lg:px-8'>
+
         {/* left col */}
+        <div className='w-full md:w-1/2 space-y-8'>
+
+        </div>
+
+
         {/* star badge */}
+
+
+
         {/* right co */}
         <div className="w-full md:w-1/2">
 
