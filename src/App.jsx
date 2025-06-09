f6b963f3a890a658cef2c1acf45d6737bdba66e2
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-10 01:20:59 +0200

    :art: Working On App  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <main className='relative min-h-screen. overflow-x-hidden'>
       <div className='absolute -top-28 -left-28 w-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20
-        rounded-full'>
+        rounded-full blur-[80px] -z-10'>
         <Navbar/>
         <Hero/>
       </div>
