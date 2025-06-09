7d417a2db54c76ca8985b6d8720bed90ceaa2a55
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-10 01:22:25 +0200

    :art: Working on App  :art:

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <div className='absolute -top-28 -left-28 w-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20
         rounded-full blur-[80px] -z-10'>
       </div>
-      <div>
+      <div className='overflow-hidden'>
         <Navbar/>
         <Hero/>
       </div>
