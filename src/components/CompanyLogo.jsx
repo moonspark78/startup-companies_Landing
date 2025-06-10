e6ca361ca11235727be072ada254c4011a975e75
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-11 00:28:03 +0200

    :art: Working on the CompanyLogo  :art:

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -5,7 +5,8 @@ const CompanyLogo = () => {
   return (
     <div className='w-full container mx-auto py-20 px-4 sm:px-6 lg:px-8 flex sm:flex-row flex-col sm:items-center
     items-start'>
-        <div>
+        <div className='w-[300px] shrink-0 px-5 text-gray-600 border-l-4 border-blue-500 bg-white py-2
+        z-10 sm:text-base text-xl font-semibold text-left'>
             Proud partern at <br/> HubSpot & Segment
         </div>
     </div>
