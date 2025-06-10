da919f2541de1725b50ff44edadc6a474c243541
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-11 00:30:48 +0200

    :art: Working On The companyLogo  :art:

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import slack from '../assets/slack.png'
+import amazon from '../assets/amazon.png'
+import woocommerce from '../assets/woocommerce.png'
+import meundies from '../assets/meundies.png'
+import sitepoint from '../assets/sitepoint.png'
 
 const CompanyLogo = () => {
     const logos = [slack, amazon, woocommerce, meundies, sitepoint]
