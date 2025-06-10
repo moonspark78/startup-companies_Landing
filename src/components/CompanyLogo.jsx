78a8d69713c5c3933fff4fd45fd780ab36d7bbeb
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-11 00:24:59 +0200

    :art: working On The CompanyLogo  :art:

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
 const CompanyLogo = () => {
+    const logos = [FaSlack, FaAmazon, woocommerce, meundies, sitepoint]
   return (
-    <div>CompanyLogo</div>
+    <div className='w-full container mx-auto py-20 px-4 sm:px-6 lg:px-8 flex sm:flex-row flex-col sm:items-center
+    items-start'>
+
+    </div>
   )
 }
 
