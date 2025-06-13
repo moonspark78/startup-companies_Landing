a0c3386e8d5a10baab543f83019f67be37db675c
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-13 18:20:42 +0200

    :art: Working On The companyLogoo   :art:

--- src/components/CompanyLogo.jsx
+++ src/components/CompanyLogo.jsx
@@ -8,7 +8,7 @@ import sitepoint from '../assets/sitepoint.png'
 const CompanyLogo = () => {
     const logos = [slack, amazon, woocommerce, meundies, sitepoint]
   return (
-    <div className='w-full container mx-auto py-20 px-4 sm:px-6 lg:px-8 flex sm:flex-row flex-col sm:items-center
+    <div className='w-full overflow-hidden container mx-auto py-20 px-4 sm:px-6 lg:px-8 flex sm:flex-row flex-col sm:items-center
     items-start'>
         <div className='w-[300px] shrink-0 px-5 text-gray-600 border-l-4 border-blue-500 bg-white py-2
         z-10 sm:text-base text-xl font-semibold text-left'>
