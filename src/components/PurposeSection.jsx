dec95b3aed9c3da78c3f5abbd08770be2bb0125e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-14 14:10:42 +0200

    :art: working  The PurposeSection component  :art:

--- src/components/PurposeSection.jsx
+++ src/components/PurposeSection.jsx
@@ -4,9 +4,11 @@ const PurposeSection = () => {
   return (
     <section className='w-full bg-gray-50 py-16 px-4 sm:px-6 md:px-8'>
     <div className="max-w-6xl mx-auto ">
+
+        {/* Heading text */}
         <div>
             <p className="text-sm text-purple-600 font-medium mb-2">ACHIEVE MORE</p>
-            <h2 className="text-3xl md:w-4/5">
+            <h2 className="text-3xl md:w-4/5 w-full md:text-4xl font-bold text-gray-900">
                 Purpose of a convoy is to keep your team
             </h2>
         </div>
