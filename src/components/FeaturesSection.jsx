60ed3f395449e7b2fa997089afb0a7e14de9e619
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-16 22:53:11 +0200

    :art: adding  the FeaturesSection components +  data  :art:

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
 
+
+  const feature = [
+    {
+      icon: "🔍", 
+      title: "Find out what you need",
+      description: "We present you a proposal and discuss nitty-gritty like"
+    },
+    {
+      icon: "⚙️",
+      title: "Work out the details", 
+      description: "Communication protocols apart from engagement models"
+    },
+    {
+      icon: "🚀",
+      title: "We get to work fast",
+      description: "Protocols apart from engage models, pricing billing"
+    }
+  ]
+
 const FeaturesSection = () => {
   return (
     <div>FeaturesSection</div>
