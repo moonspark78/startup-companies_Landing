494f38dc6165d8176d3b11db889e9644949e15f1
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-18 23:25:41 +0200

    :art: working  the FeaturesSection components / :art:

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -45,7 +45,8 @@ const FeaturesSection = () => {
 
         {/* button */}
         <div className='text-center mt-12'>
-          <button className='bg-blue-600'>
+          <button className='bg-blue-600 text-white cursor-pointer px-8 py-3 rounded-full font-medium
+          hover:bg-blue-700'>
 
           </button>
 
