523ac9539fada9632626ed7ca0eec4417e377591
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-18 23:26:59 +0200

    :art: working  the FeaturesSection componenst  :art:

--- src/components/FeaturesSection.jsx
+++ src/components/FeaturesSection.jsx
@@ -48,7 +48,7 @@ const FeaturesSection = () => {
           <button className='bg-blue-600 text-white cursor-pointer px-8 py-3 rounded-full font-medium
           hover:bg-blue-700 transition-colors relative'>
             Become a Partner
-            <div></div>
+            <div className='absolute -z-10 w-full'></div>
           </button>
 
         </div>
