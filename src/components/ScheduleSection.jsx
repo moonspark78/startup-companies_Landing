bfe3dd43a3b8914c887f94f2d3db54e7df6d219e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-19 22:47:50 +0200

    :art: working  the FeaturesSection componenst    :art:

--- src/components/ScheduleSection.jsx
+++ src/components/ScheduleSection.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ScheduleSection = () => {
   return (
     <section className='max-w-7xl mx-auto px-4 py-16 md:py-24'>
-        <div className='flex flex-col'>
+        <div className='flex flex-col md:flex-row'>
 
         </div>
     </section>
