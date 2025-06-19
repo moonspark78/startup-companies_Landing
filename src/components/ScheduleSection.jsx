3e345c44b68dcb131a8d17c16a46259a47ee32f7
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-06-19 22:49:48 +0200

    :art: working  the FeaturesSection componenst    :art:

--- src/components/ScheduleSection.jsx
+++ src/components/ScheduleSection.jsx
@@ -5,6 +5,11 @@ const ScheduleSection = () => {
     <section className='max-w-7xl mx-auto px-4 py-16 md:py-24'>
         <div className='flex flex-col md:flex-row items-center justify-between gap-12 md:gap-24'>
 
+            {/* Left side content */}
+            <div className='md:w-1/2 w-full'>
+                <img/>
+            </div>
+
         </div>
     </section>
   )
