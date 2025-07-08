168863a954af564b4ce63450d04dbfd3134a8a67
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-08 23:58:16 +0200

    working  the servicesSection  :  :art:

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -6,7 +6,7 @@ const ServicesSection = () => {
         <div>
             {/* header */}
             <div>
-                <h2>Future of support with new shape</h2>
+                <h2 className='text-3xl md:text-4xl font-bold mb-6 md:w-4/5'>Future of support with new shape</h2>
             </div>
         </div>
     </section>
