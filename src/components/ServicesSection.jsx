dbf031f86e58b65e35600591f116face664e34fc
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-09 00:01:02 +0200

    working  the servicesSections    :art:

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -7,6 +7,7 @@ const ServicesSection = () => {
             {/* header */}
             <div>
                 <h2 className='text-3xl md:text-4xl font-bold mb-6 md:w-4/5'>Future of support with new shape</h2>
+                <p className='text-gray-500 text-lg mb-4 md:w-4/5'>Discuss your goals, determine success</p>
             </div>
         </div>
     </section>
