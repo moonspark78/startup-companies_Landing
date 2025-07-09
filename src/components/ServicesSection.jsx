fbc52f19135ccb7bb4f97db31cc4dcac5c871028
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-09 20:55:07 +0200

    working  the servicesSections :/  :art:

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -21,7 +21,7 @@ const ServicesSection = () => {
                         <div className='w-5 h-5 rounded-full bg-indigo-100 flex items-center justify-center'>
                             <div className='w-2.5 h-2.5 rounded-full bg-indigo-600'></div>
                         </div>
-                        <span className='text-gray-600'>UX design contnet strategy</span>
+                        <span className='text-gray-600'>Development bring</span>
                     </div>
                 </div>
             </div>
