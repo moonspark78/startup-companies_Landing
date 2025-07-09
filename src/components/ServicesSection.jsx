447c727f9e311972569100cd478479a1c4755a60
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-09 20:52:24 +0200

    add /.  :art:

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -16,6 +16,13 @@ const ServicesSection = () => {
                         </div>
                         <span className='text-gray-600'>UX design contnet strategy</span>
                     </div>
+                    
+                    <div className='flex items-center gap-2'>
+                        <div className='w-5 h-5 rounded-full bg-indigo-100 flex items-center justify-center'>
+                            <div className='w-2.5 h-2.5 rounded-full bg-indigo-600'></div>
+                        </div>
+                        <span className='text-gray-600'>UX design contnet strategy</span>
+                    </div>
                 </div>
             </div>
         </div>
