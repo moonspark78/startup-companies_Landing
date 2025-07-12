58c08d51af9329549aada35b6b557c5dcc73279a
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-12 12:34:34 +0200

    working  the servicesSection    :art:

--- src/components/ServicesSection.jsx
+++ src/components/ServicesSection.jsx
@@ -1,4 +1,36 @@
 import React from 'react'
+import { BiTime } from 'react-icons/bi';
+import { BsStack } from 'react-icons/bs';
+import { FiSettings } from 'react-icons/fi';
+import { HiLightBulb } from 'react-icons/hi';
+
+//   services section
+const services = [
+    {
+      icon: <BsStack className="w-8 h-8 text-indigo-600" />,
+      title: "Web Design",
+      description: "One for all and all for one, Muskehounds are always ready.",
+      link: "#learn-more"
+    },
+    {
+      icon: <HiLightBulb className="w-8 h-8 text-amber-400" />,
+      title: "Ad-Creatives", 
+      description: "Alphabet Village and the subline of her own road.",
+      link: "#learn-more"
+    },
+    {
+      icon: <FiSettings className="w-8 h-8 text-red-400" />,
+      title: "Automation",
+      description: "Little Blind Text should turn around and return.",
+      link: "#learn-more"
+    },
+    {
+      icon: <BiTime className="w-8 h-8 text-cyan-400" />,
+      title: "Infographics",
+      description: "Nothing the copy said could convince her.",
+      link: "#learn-more"
+    }
+  ]
 
 const ServicesSection = () => {
   return (
