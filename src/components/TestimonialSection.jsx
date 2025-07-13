c96fcb61e6f439879072ab2c1df0b76dda24868e
moonspark78 <115048076+moonspark78@users.noreply.github.com>
2025-07-13 19:31:06 +0200

    working  the TestimonialSection    :art:

--- src/components/TestimonialSection.jsx
+++ src/components/TestimonialSection.jsx
@@ -1,16 +1,60 @@
-
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/navigation';
+import "swiper/css";
+import "swiper/css/navigation";
 
 // import required modules
-import { Pagination } from 'swiper/modules';
+import { Pagination } from "swiper/modules";
 
 const TestimonialSection = () => {
   return (
-    <div>TestimonialSection</div>
-  )
-}
+    <section>
+      <div>
+        <h2></h2>
+        <p></p>
+      </div>
 
-export default TestimonialSection
+      {/* testimonial cards */}
+      <div>
+        {/* swiper cards */}
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          pagination={{
+            clickable: true,
+          }}
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+
+        {/* navigation button */}
+      </div>
+    </section>
+  );
+};
+
+export default TestimonialSection;
