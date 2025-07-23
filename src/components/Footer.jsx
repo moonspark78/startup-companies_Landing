eae385ebea2534cad1bb68f3b87645c4be29ab18
Omar <jabour.omar@hotmail.com>
2025-07-23 21:23:07 +0200

    working in progress

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='bg-gray-50'>
+        <div>
+            <div>
+                {/* rand column */}
+            </div>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
