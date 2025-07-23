80114af77c3350e134b76989a8aba883657b99a4
Omar <jabour.omar@hotmail.com>
2025-07-23 21:23:46 +0200

    working in progress ./

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className='bg-gray-50'>
-        <div>
+        <div className='container mx-auto px-4 sm:px-6 lg:px-8 py-16'>
             <div>
                 {/* rand column */}
             </div>
