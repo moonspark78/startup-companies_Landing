fb98031fb8cbf9ea101c9d9c5f389140c1a3925e
Omar <jabour.omar@hotmail.com>
2025-07-25 22:35:12 +0200

    working in the footer /: first icon social  :art:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaFacebook } from "react-icons/fa";
+
 
 const Footer = () => {
   return (
@@ -13,8 +15,17 @@ const Footer = () => {
               <div className="w-4 h-4 bg-red-500 -ml-2 rounded-full opacity-100 hover:opacity-75 transition-opacity"></div>
             </div>
             <span className="text-xl font-medium ml-1">The Next Desugn</span>
-          </div>
-          <p className="text-gray-600 mb-6">The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times.</p>
+            </div>
+            <p className="text-gray-600 mb-6">
+              The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times.
+            </p>
+
+            <div>
+              <a className="w-10 h-10 bg-gray-200 rounded-full flex items-center justify-center text-gray-600
+               hover:bg-blue-500 hover:text-white transition-all duration-200">
+                <FaFacebook className="size-5" />
+              </a>
+            </div>
           </div>
         </div>
       </div>
