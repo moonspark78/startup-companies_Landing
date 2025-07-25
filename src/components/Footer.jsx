0a3d55c029f985029911f99a03809cdea7ea7e22
Omar <jabour.omar@hotmail.com>
2025-07-25 21:29:17 +0200

    working in the footer : :art:

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
       <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-16">
         <div>
           {/* rand column */}
-          <div>
+          <div className="flex gap-1 items-center">
             <div className="flex items-center gap-1 cursor-pointer">
               <div className="w-4 h-4 bg-blue-600 rounded-full opacity-75 hover:opacity-100 transition-opacity"></div>
               <div className="w-4 h-4 bg-red-500 -ml-2 rounded-full opacity-100 hover:opacity-75 transition-opacity"></div>
